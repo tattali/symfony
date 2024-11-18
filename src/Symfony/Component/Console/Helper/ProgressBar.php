6983de86616dbd0eb1cde9c417ae16688eef72bb
Théo Attali <theoattali@gmail.com>
2024-11-18 10:35:55 +0100

    Fix code style

--- src/Symfony/Component/Console/Helper/ProgressBar.php
+++ src/Symfony/Component/Console/Helper/ProgressBar.php
@@ -67,13 +67,7 @@ final class ProgressBar
     /**
      * @param int $max Maximum steps (0 if unknown)
      */
-    public function __construct(
-        OutputInterface $output, 
-        int $max = 0, 
-        float $minSecondsBetweenRedraws = 1 / 25,
-        ?string $message = null,
-        ?string $format = null,
-    ) {
+    public function __construct(OutputInterface $output, int $max = 0, float $minSecondsBetweenRedraws = 1 / 25, ?string $message = null, ?string $format = null) {
         if ($output instanceof ConsoleOutputInterface) {
             $output = $output->getErrorOutput();
         }
